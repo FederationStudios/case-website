34f00ef1a8824b34dbf071fbb113c3a131bb7528
suman9725 <sounakofficial448@gmail.com>
2024-08-03 14:53:24 +0530

    Updated to CoA

--- DBModels/Case.js
+++ DBModels/Case.js
@@ -1,30 +1,57 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const caseSchema = new mongoose.Schema({
     case_id: {
-      type: String,
-      required: true,
-      unique: true // Ensure this field is unique
+        type: String,
+        required: true,
+        unique: true
     },
-    submitter: String,
-    offender: String,
-    incident: String,
-    violations: String,
-    evidence: String,
+    discord_username: {
+        type: String,
+        required: true
+    },
+    offense: {
+        type: String,
+        required: true
+    },
+    case_link: {
+        type: String,
+        default: 'N/A'
+    },
+    punishment: {
+        type: String,
+        required: true
+    },
+    background: {
+        type: String,
+        required: true
+    },
+    in_person_or_discord: {
+        type: String,
+        required: true
+    },
+    deserved_or_not: {
+        type: String,
+        required: true
+    },
     status: {
-      type: String,
-      default: "Pending"
+        type: String,
+        enum: ['PENDING', 'DENIED', 'PENDING APPROVAL FROM COA COMMAND', 'ON HOLD', 'AWAITING ASSIGNMENT', 'COMPLETED'],
+        default: 'PENDING'
     },
-    guild_id: {
-      type: String,
-      required: true
+    judges_assigned: {
+        type: Boolean,
+        default: false
+    },
+    judges_username: {
+        type: String,
+        default: 'N/A'
     },
     submitted_date: {
         type: Date,
         default: Date.now
     }
-  });  
+});
 
-const Case = mongoose.model("Case", caseSchema);
-
-module.exports = Case;
+const case_list = mongoose.model('Case', caseSchema);
+module.exports = case_list;
